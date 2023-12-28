76d03d4a23db0830ffa12a5752b31c9501f1fcae
Ryan Tran <ryyt@capgroup.com>
2023-12-28 11:47:42 -0800

    feat: remove un-used imports

--- beginning-bazel/java-protobuf/src/main/java/com/raejz/firstbuf/EchoClient.java
+++ beginning-bazel/java-protobuf/src/main/java/com/raejz/firstbuf/EchoClient.java
@@ -2,7 +2,6 @@ package com.raejz.firstbuf;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 import transmission_object.TransmissionObjectOuterClass.TransmissionObject;
 
