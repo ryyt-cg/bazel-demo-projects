50ffe1b4699c2029468ac3f9f194ca9040ff4d29
Ryan Tran <ryyt@capgroup.com>
2023-12-29 14:39:22 -0800

    feat: protobuf implementation works and java grpc cleint works

--- beginning-bazel/java-grpc-client/src/main/java/com/raejz/grpc/EchoClient.java
+++ beginning-bazel/java-grpc-client/src/main/java/com/raejz/grpc/EchoClient.java
@@ -1,34 +1,42 @@
-package com.raejz.firstbuf;
+package com.raejz.grpc;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.Socket;
+import transceiver.TransceiverGrpc;
+import transceiver.TransceiverOuterClass.EchoRequest;
+import transceiver.TransceiverOuterClass.EchoResponse;
 import transmission_object.TransmissionObjectOuterClass.TransmissionObject;
 
+//@Slf4j
 public class EchoClient {
-
   public static void main(String args[]) {
     System.out.println("Spinning up the Echo Client in Java...");
     try {
-      final Socket socketToServer = new Socket("localhost", 1234);
-      // Note we don't need the second BufferedReader here.
       final BufferedReader commandLineInput = new BufferedReader(new InputStreamReader(System.in));
       System.out.println("Waiting on input from the user...");
       final String inputFromUser = commandLineInput.readLine();
       if (inputFromUser != null) {
-        System.out.println("Received by Java: " + inputFromUser);
-        TransmissionObject transmissionObject = TransmissionObject.newBuilder()
-            .setMessage(inputFromUser)
-            .setValue(3.145f)
+        ManagedChannel channel =
+            ManagedChannelBuilder
+                .forAddress("localhost", 1234)
+                .usePlaintext()
+                .build();
+        TransceiverGrpc.TransceiverBlockingStub stub =
+            TransceiverGrpc.newBlockingStub(channel);
+        EchoRequest request =  EchoRequest.newBuilder()
+            .setFromClient(
+                TransmissionObject.newBuilder()
+                    .setMessage(inputFromUser)
+                    .setValue(3.145f)
+                    .build())
             .build();
-
-        transmissionObject.writeTo(socketToServer.getOutputStream());
-        TransmissionObject receivedObject = TransmissionObject.parseFrom(
-            socketToServer.getInputStream());
+        EchoResponse response = stub.echo(request);
         System.out.println("Received Message from server: ");
-        System.out.println(receivedObject);
+        System.out.println(response);
+        channel.shutdownNow();
       }
-      socketToServer.close();
     } catch (Exception e) {
       System.err.println("Error: " + e);
     }
