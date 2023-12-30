1c9560f2947b7cba2dde0469e2a7b036eddc1aa0
Ryan Tran <ryyt@capgroup.com>
2023-12-29 17:24:32 -0800

    feat: gofmt go files

--- beginning-bazel/go-echo-server/echo_server.go
+++ beginning-bazel/go-echo-server/echo_server.go
@@ -7,24 +7,28 @@ import (
 
 func main() {
 	log.Println("Spinning up the Echo Server in Go...")
-	listen, error := net.Listen("tcp", ":1234")
-	if error != nil {
-		log.Panicln("Unable to listen: " + error.Error())
+	listen, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Panicln("Unable to listen: " + err.Error())
 	}
+
 	defer listen.Close()
-	connection, error := listen.Accept()
-	if error != nil {
-		log.Panicln("Cannot accept a connection! Error: " + error.Error())
+	connection, err := listen.Accept()
+	if err != nil {
+		log.Panicln("Cannot accept a connection! Error: " + err.Error())
 	}
+
 	log.Println("Receiving on a new connection")
 	defer connection.Close()
 	defer log.Println("Connection now closed.")
 	buffer := make([]byte, 2048)
-	size, error := connection.Read(buffer)
-	if error != nil {
-		log.Println("Cannot read from the buffer! Error: " + error.Error())
+
+	size, err := connection.Read(buffer)
+	if err != nil {
+		log.Println("Cannot read from the buffer! Error: " + err.Error())
 	}
 	data := string(buffer[:size])
+
 	log.Println("Received data: " + data)
 	connection.Write([]byte("Echoed from Go: " + data))
 }
--- beginning-bazel/go-grpc-server/main.go
+++ beginning-bazel/go-grpc-server/main.go
@@ -16,33 +16,34 @@ func (es *EchoServer) Echo(context context.Context, request *transceiver.EchoReq
 	log.Println("Message = " + (*request).FromClient.GetMessage())
 	log.Println("Value = " +
 		fmt.Sprintf("%f", (*request).FromClient.GetValue()))
-	server_message := "Received from client: " +
+	serverMessage := "Received from client: " +
 		(*request).FromClient.GetMessage()
-	server_value := (*request).FromClient.Value * 2
-	from_server := transmission.TransmissionObject{
-		Message: server_message,
-		Value:   server_value,
+	serverValue := (*request).FromClient.Value * 2
+	fromServer := transmission.TransmissionObject{
+		Message: serverMessage,
+		Value:   serverValue,
 	}
 
 	return &transceiver.EchoResponse{
-		FromServer: &from_server,
+		FromServer: &fromServer,
 	}, nil
 }
 
 func main() {
 	log.Println("Spinning up the Echo Server in Go...")
-	listen, error := net.Listen("tcp", ":1234")
-	if error != nil {
-		log.Panicln("Unable to listen: " + error.Error())
+	listen, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Panicln("Unable to listen: " + err.Error())
 	}
+
 	defer listen.Close()
 	defer log.Println("Connection now closed.")
 	grpc_server := grpc.NewServer()
 
 	transceiver.RegisterTransceiverServer(grpc_server, &EchoServer{})
 
-	error = grpc_server.Serve(listen)
-	if error != nil {
-		log.Panicln("Unable to start serving! Error: " + error.Error())
+	err = grpc_server.Serve(listen)
+	if err != nil {
+		log.Panicln("Unable to start serving! Error: " + err.Error())
 	}
 }
